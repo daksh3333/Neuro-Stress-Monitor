0fa1b719f6d73dfde5509cefd0f1931eb1a0bc04
Betts6430 <avery.bettesworth643@gmail.com>
2024-11-16 13:02:39 -0700

    Update stress_monitor.py
    updated to use eeg data

--- stress_monitor.py
+++ stress_monitor.py
@@ -1,4 +1,4 @@
-# import serial 
+import serial 
 import time 
 from flask import Flask
 from flask_socketio import SocketIO, emit
@@ -9,21 +9,21 @@ load_dotenv()
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
-# arduino = serial.Serial(port='COM3', baudrate=115200, timeout=2) 
+arduino = serial.Serial(port='COM3', baudrate=115200, timeout=2) 
 
 def write_read(): 
 	# arduino.write(bytes(x, 'utf-8')) 
 	# time.sleep(0.05) 
 	while True:
 		try:
-			# data = arduino.readline() 
-			data = 1
+			data = arduino.readline() 
+			# data = 1
 			if data:
-				# output = int(data.decode('utf-8').strip())
-				output = data
+				output = int(data.decode('utf-8').strip())
+				# output = data
 				socketio.emit('arduino_data', {'value': output})
 				print(f"Sent: {output}")
-			time.sleep(1)  # Add a delay between readings to avoid flooding
+			time.sleep(0.1)  # Add a delay between readings to avoid flooding
 		except Exception as e:
 			print(f"Error reading from Arduino: {e}")
 
