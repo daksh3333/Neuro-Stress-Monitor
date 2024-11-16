4cc4d862075a1675161cb907b33c7afcdbebd9d6
Betts6430 <avery.bettesworth643@gmail.com>
2024-11-15 19:02:10 -0700

    Update stress_monitor.py

--- stress_monitor.py
+++ stress_monitor.py
@@ -1,1 +1,12 @@
-
+import serial 
+import time 
+arduino = serial.Serial(port='COM3', baudrate=115200, timeout=2) 
+def write_read(x): 
+	# arduino.write(bytes(x, 'utf-8')) 
+	# time.sleep(0.05) 
+	data = arduino.readline() 
+	return data 
+while True: 
+	num = 4 #input("Enter a number: ") # Taking input from user 
+	value = write_read(num) 
+	print(value) # printing the value
